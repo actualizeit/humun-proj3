76f623569c82cf1c9586f1e7bb5c3eb56be7ff25
acarlie <ameliacarlie@gmail.com>
2019-11-23 11:07:50 -0500

    Made changes to users.js api routes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,39 +4,26 @@ const appController = require('../../controllers/appController');
 const passport = require('passport');
 
 // Register
-router.post('/register', (req, res) => {
-  appController.register(req, res);
-});
+router.post('/register', appController.register);
 
 // Login
-router.post('/login', (req, res, next) => {
-  appController.login(req, res);
-});
+router.post('/login', appController.login);
 
 // Test Json Web Token not expired
 router.get('/test', passport.authenticate('jwt', { session: false }), (req, res, next) => {
   res.json({ success: true });
 });
 
-// Get user data
-router.get('/mydata', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  appController.findUserData(req, res);
-});
-
-// Post or update user data
-router.post('/mydata', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  console.log('test');
-  appController.saveUserData(req, res);
-});
-
+// Retrieve user data
 router
-  .post('/mydata/:id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-    console.log('test');
-    appController.updateUser(req, res);
-  })
-  .put('/mydata/:id', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-    console.log('test');
-    appController.updateUser(req, res);
-  });
+  .route('/mydata')
+  .get(passport.authenticate('jwt', { session: false }), appController.findUserData)
+  .post(passport.authenticate('jwt', { session: false }), appController.saveUserData);
+
+// Update specific user data, Post specific user data
+router
+  .route('/mydata/:id')
+  .post(passport.authenticate('jwt', { session: false }), appController.saveUserData)
+  .put(passport.authenticate('jwt', { session: false }), appController.updateUser);
 
 module.exports = router;
